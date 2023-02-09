4ea4032c38bc38ca4916c3120c04847e0cc9c139
Ben Ambler <benjaminambler@mail.weber.edu>
2023-02-09 00:14:23 -0700

    Lab 4, text based camel game

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -2,6 +2,7 @@
 chased text based game written by Ben Ambler 2/6/2023 """
 
 import random
+'''
 def print_instructions():
     """ This function prints the instructions """
 
@@ -11,33 +12,114 @@ def print_instructions():
     The natives want their camel back and are chasing you down! Survive your
     desert trek and out run the natives.
     """)
+'''
+'''
+def print_choices():
+    print("""       
+                    A. Drink from your canteen. 
+                    B. Ahead moderate speed.
+                    C. Ahead full speed.
+                    D. Stop for the night.
+                    E. Status check.
+                    Q. Quit.""")
+'''
+# set your variables
+miles_traveled = 0
+thirst = 0
+camel_tiredness = 0
+miles_natives_traveled = -20
+drinks_left_in_canteen = 3
 
-    """ Creating a Boolean value called done and setting it to False """
+# print_instructions():
 
-def get_player_choice():
-        """ Get the player's choice """
-        print("""       A. Drink from your canteen. 
-                        B. Ahead moderate speed.
-                        C. Ahead full speed.
-                        D. Stop for the night.
-                        E. Status check.
-                        Q. Quit.""")
-        # Keep looking until someone wins
-        done = False
-        while not done:
-            # Loop for each player
-            for player_choice in player_names:
-                # Process their turn
-                done = process_player_turn(player_name, distance_apart)
-                # If someone won, 'break' out of this loop and end the game.
-                if done:
-                    break
+print("""
+    Welcome to Camel!
+    You have stolen a camel to make your way across the great Mobi desert.
+    The natives want their camel back and are chasing you down! Survive your
+    desert trek and out run the natives.""")
 
+# get_player_choice():
+done = False
+while not done:
+    print("""       
+                    A. Drink from your canteen. 
+                    B. Ahead moderate speed.
+                    C. Ahead full speed.
+                    D. Stop for the night.
+                    E. Status check.
+                    Q. Quit.
+    """)
+    player_choice = input("What is your choice? ")
+
+    if player_choice.upper() == "Q":
+        done = True
+
+    elif player_choice.upper() == "E":
+        print("You've gone this far:", miles_traveled)
+        print("You've got this many drinks left:", drinks_left_in_canteen)
+        print("The natives are", miles_traveled - miles_natives_traveled, "miles back ")
+        print("Your camel tiredness is", camel_tiredness)
+        print("You are now this thirsty", thirst)
+
+    elif player_choice.upper() == "D":
+        camel_tiredness = 0
+        print("Your camel yawns, stretches, and looks happy and refreshed")
+        miles_natives_traveled = miles_natives_traveled + random.randrange (7,15)
+
+    elif player_choice.upper() == "C":
+        miles_traveled = miles_traveled + random.randrange (10, 21)
+        print("you traveled", miles_traveled, "miles")
+        thirst = thirst + 1
+        camel_tiredness = camel_tiredness + random.randrange (1,4)
+        miles_natives_traveled = miles_natives_traveled + random.randrange (7, 15)
+        print("You are now", miles_traveled - miles_natives_traveled, "miles ahead of the natives")
+
+    elif player_choice.upper() == "B":
+        miles_traveled = miles_traveled + random.randrange(5, 13)
+        print("you traveled", miles_traveled, "miles")
+        thirst = thirst + 1
+        camel_tiredness = camel_tiredness + random.randrange(1, 4)
+        miles_natives_traveled = miles_natives_traveled + random.randrange(7, 15)
+
+    elif player_choice.upper() == "A":
+        if drinks_left_in_canteen > 0:
+            drinks_left_in_canteen = drinks_left_in_canteen - 1
+            thirst = 0
+            print("""
+                    You carefully take a sip from your canteen, being extra careful
+                    to not spill even a single drop. 
+                    Your thirst is quenched and you feel refreshed. """)
+        elif drinks_left_in_canteen == 0:
+            print(""" You are all out of water... this is not good. """)
+    if done == False and camel_tiredness > 5:
+        print("Your camel is getting tired")
+    if done == False and thirst > 4:
+            print(""" You are very thirsty. """)
+    if thirst > 6:
+        print(""" You died of thirst. Game Over. """)
+        done = True
+    if done == False and camel_tiredness > 8:
+        print("Your camel died")
+        done = True
+
+
+
+
+
+
+
+
+
+
+
+'''
 def main():
     """ Main Program """
     print_instructions()
+    print_choices()
     get_player_choice()
 
 
 if __name__ == "__main__":
     main()
+'''
