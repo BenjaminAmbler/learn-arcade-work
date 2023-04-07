0f498077b20fe638106a2f7ce731754a4dca61f2
BenjaminAmbler <benjaminambler@gmail.com>
2023-04-06 22:44:55 -0600

    Lab 10

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -13,7 +13,7 @@ import re
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
 
-def main():
+def read_in_dictionary():
     """ Read in lines from a file """
 
     # Open the file for reading, and store a pointer to it in the new
@@ -32,7 +32,38 @@ def main():
 
     dictionary_file.close()
 
-    print( "There are", len(words_in_dictionary_list), "words in this dictionary file.")
+    # uncomment the line below to print out how many words are in the
+    # dictionary to be able to see if this thing is working so far:
+    # print( "There are", len(words_in_dictionary_list), "words in this dictionary file.")
+
+    return words_in_dictionary_list
+
+def linear_search(key, words_in_dictionary_list):
+    """ Linear search """
+
+    # Start at the beginning of the list
+    current_list_position = 0
+
+    # Loop until you reach the end of the list, or the value at the
+    # current position is equal to the key
+    while current_list_position < len(words_in_dictionary_list) and words_in_dictionary_list[current_list_position] != key:
+
+        # Advance to the next item in the list
+        current_list_position += 1
+
+    return current_list_position
+
+
+def main():
+
+    key = "Morgiana the Shrew"
+    words_in_dictionary_list = read_in_dictionary("dictionary.txt")
+    list_position = linear_search(key, words_in_dictionary_list)
+
+    if list_position < len(words_in_dictionary_list):
+        print("The name", key, "is at position", list_position)
+    else:
+        print("The name", key, "was not in the list.")
 
 
 main()
