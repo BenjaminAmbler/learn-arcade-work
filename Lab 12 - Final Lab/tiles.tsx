a85b975d5a0ad9b3e5bac0236371500f18025415
Ben Ambler <benjaminambler@gmail.com>
2023-04-12 15:06:49 -0600

    Merge remote-tracking branch 'origin/master'

---
+++ Lab 12 - Final Lab/tiles.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="32" tileheight="32" tilecount="2496" columns="52">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="../../../Downloads/tiles.png" width="1664" height="1536"/>
+</tileset>
